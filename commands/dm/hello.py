113c3f9db551a7579367c8f4252cfa1c678eff5b
John D'Alessandro <dalessandro.john.24+github.com>
2017-10-13 11:30:35 -0400

    Modular commands, adding commands is super easy

---
+++ commands/dm/hello.py
@@ -0,0 +1,8 @@
+from telegram_wrapper import *
+
+# text = update["message"]["text"]
+
+def command(t, update, args):
+    chat = update["message"]["chat"]["id"]
+    sent_from = update["message"]["from"]
+    t.send_message("Hello "+sent_from["first_name"], chat)
