51054cbf83101688c8093d5f595afdb42df31985
John D'Alessandro <dalessandro.john.24+github.com>
2017-10-17 14:53:00 -0400

    API thread restarts on crash

--- lib/api.py
+++ lib/api.py
@@ -2,13 +2,17 @@ from flask import Flask
 from lib.telegram_wrapper import *
 
 def apiThread(t, address):
-	app = Flask(__name__)
-	app.config['DEBUG'] = False
-	app.config['SERVER_NAME'] = address + ":5000"
+	try:
+		app = Flask(__name__)
+		app.config['DEBUG'] = False
+		app.config['SERVER_NAME'] = address + ":5000"
 
-	@app.route("/doorbell")
-	def doorbell():
-		t.send_message("Ding-dong, somebody is at the door", t.HOUSE_CHAT_ID)
-		return "Doorbell Rung"
+		@app.route("/doorbell")
+		def doorbell():
+			t.send_message("Ding-dong, somebody is at the door", t.HOUSE_CHAT_ID)
+			return "Doorbell Rung"
 
-	app.run(host='0.0.0.0')
+		app.run(host='0.0.0.0')
+	except Exception as e:
+		print(e)
+		apiThread(t, address)
